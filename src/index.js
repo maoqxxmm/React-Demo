3e00ad50c8301103870fba1d5c4dfe2a1e803bdd
maoq <maoqxm@appest.com>
2018-03-29 17:25:34 +0800

    redux devtools

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import rootReducer from './reducers';
 import './styles/main.scss';
 import Tasklist from './routers/tasklist';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, /* preloadedState, */ devToolsEnhancer(
+    // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+  ));
 
 ReactDOM.render(
     <Provider store={store}>
