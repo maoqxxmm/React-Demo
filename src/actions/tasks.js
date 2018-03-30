3d61682ea4e2632a6bdf54e401664d23a9f21a07
maoq <maoqxm@appest.com>
2018-03-30 18:21:33 +0800

    update

--- src/actions/tasks.js
+++ src/actions/tasks.js
@@ -1,11 +1,10 @@
 import * as types from '../constants/ActionTypes';
-
-let idCounter = 0;
+import ObjectId from '../libs/ObjectId';
 
 export const addTask = text => ({
     type: types.ADD_TASK,
     text,
-    id: ++idCounter
+    id: ObjectId()
 })
 
 export const toggleCompleteTask = id => ({
